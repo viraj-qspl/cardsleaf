5ee48f0fa53ef88383ab7b8aaedbe1e687a762ab
virajk <viraj.kamat@quagnitia.com>
2014-05-06 15:19:13 +0530

    Package Page HTML Integrated

--- application/views/frontend/package.php
+++ application/views/frontend/package.php
@@ -8,134 +8,106 @@
 	});
 </script>
 
-<div class="mid_container mid_wrapper">
 
 
-    <div class="yourpackage_container">
-	<div class="msg_box"><?php echo $this->session->userdata('pack_exp_msg'); ?></div>
-        <div class="your_package">
-            <div class="pack">
-                <div class="item1">Select your package</div>
-                <div class="item2">Monthly</div>
-                <div class="item2">Semi- Annually</div>
-                <div class="item2">Annually</div>
-            </div>
-            <div class="bnank_p">&nbsp;</div>
-            <div class="pack">
-                <div class="item1">What you get</div>
-                <div class="item2">Each card is uniquely designed by you</div>
-                <div class="item2">Personalized message deliver to friends and family</div>
-                <div class="item2">Personal touch </div>
-            </div>
-            <div class="bnank_p">&nbsp;</div>
-            <div class="pack">
-                <div class="item1">Why you should join</div>
-                <div class="item2">Paper cards brings priceless smile which no online card can bring</div>
-                <div class="item2">Card shipment within 48hrs of order</div>
-                <div class="item2">Sign in and add the date of card delivery and we take care rest of things for you</div>
-            </div>
-        </div>
-       <div class="your_package">
-	    
-<!--        <div class="packa">
-            	<div class="packlist">
-                	<div class="item1">Free</div>
-                	<div class="item3">$0</div>
-                	<div class="item2">
-			    <p>Unlimited online cards</p>
-			</div>
-                </div>
-                <div class="packbutton"><a href="#">ACTIVITATE NOW</a></div>
-            </div>
-	    
-            <div class="bnank_p">&nbsp;</div>
-	    
-            <div class="packa">
-            	<div class="packlist">
-                    <div class="item1">Month – Month club</div>
-                    <div class="item3">$4.99/Month</div>
-                    <div class="item2">
-			<p>Unlimited online cards</p>
-			<p>Send 3 Personalized cards a month</p>
-			<p>100% satisfaction guarantee</p>
-			<p>Local Postage Included</p>
-		    </div>
-                </div>
-                <div class="packbutton"><a href="#">BUY NOW</a></div>
-            </div>
-	    
-            <div class="bnank_p">&nbsp;</div>
-	    
-            <div class="packa">
-            	<div class="packlist">
-                    <div class="item1">6 Months club</div>
-                    <div class="item3">$25 for 6 months 3 Cards per month</div>
-                    <div class="item2">
-			<p>Unlimited online cards</p>
-			<p>Send 3 Personalized cards a month</p>
-			<p>Pay for long term and save more</p>
-			<p>100% satisfaction guarantee</p>
-			<p>Local Postage Included</p>
-		    </div>
-                    
-                </div>
-                <div class="packbutton"><a href="#">BUY NOW</a></div>
-            </div>
-	    
-            <div class="bnank_p">&nbsp;</div>
-	    
-            <div class="packa">
-            	<div class="packlist">
-                    <div class="item1">One Year club</div>
-                    <div class="item3">$40 for one year 3 Cards per month And roll over Option</div>
-                    <div class="item2">
-			<p>Unlimited online cards</p>
-			<p>Send 3 Personalized cards a month</p>
-			<p>Pay for a year and get roll over up to 6 cards</p>
-			<p>Pay for long term and save more</p>
-			<p>100% satisfaction guarantee</p>
-			<p>Local Postage Included</p>
-			<p>$19.88 saving from month – month subscription</p>
-		    </div>
-                </div>
-                <div class="packbutton"><a href="#">BUY NOW</a></div>
-            </div>-->
-	    
-	    <?php for($i=0;$i<sizeof($packsDtls);$i++) {?>
-	    <div class="packa">
-            	<div class="packlist">
-                	<div class="item1"><?php echo $packsDtls[$i]['pack_title'];?></div>
-                	<div class="item3"><?php echo $packsDtls[$i]['pack_brief'];?></div>
-                	<div class="item2">
-			    <?php echo html_entity_decode($packsDtls[$i]['pack_desc']);?>
-			</div>
-                </div>
-				
 
 
-                <div class="packbutton"><a style="cursor: pointer;" q='<?php echo base64_encode($packsDtls[$i]['pid']);?>' >
-				<?php 
-				
-			
-								
+
+<div class="container">
+
+<div class="msg_box"><?php echo $this->session->userdata('pack_exp_msg'); ?></div>
+
+<div class="content distance">
+<h2>Select Package</h2>
+
+<div class="package clearfix">
+<div class="pack">
+<h2>Select Package</h2>
+
+<ul>
+<li>Monthly</li>
+<li>Semi-Annually</li>
+<li>Annually</li>
+</ul>
+</div><!--pack-->
+
+<div class="pack">
+<h2>What you get</h2>
+
+<ul>
+<li>Each card is uniquely designed by you</li>
+<li>Personalized message deliver to friends and family</li>
+<li>Personal touch</li>
+</ul>
+</div><!--pack-->
+
+
+<div class="pack">
+<h2>Why you should join</h2>
+
+<ul>
+<li>Paper cards brings priceless smile which no online card can bring</li>
+<li>Card shipment within 48hrs of order</li>
+<li>Sign in and add the date of card delivery and we take care rest of things for you</li>
+</ul>
+</div><!--pack-->
+<div class="clear"></div>
+</div>
+
+<div class="package_set">
+
+
+<?php for($i=0;$i<sizeof($packsDtls);$i++) {?>
+<div class="acc">
+
+
+		<div class="account">
+		<div class="head_set"><?php echo $packsDtls[$i]['pack_title'];?></div>
+		<div class="dolar"><?php echo $packsDtls[$i]['pack_brief'];?></div>
+
+		<div class="para">
+		<?php echo html_entity_decode($packsDtls[$i]['pack_desc']);?>
+		</div><!--para-->
+		</div><!--account-->
+		<div class="clear"></div>
+		<div class="button"><a style="cursor: pointer;" q='<?php echo base64_encode($packsDtls[$i]['pid']);?>'><div class="now_btn">
+		
+			<?php
 				if($packsDtls[$i]['pid']==$user_details['packagechoose'] && $user_details['cardsend']>0)
 					echo 'ACTIVATED NOW';  
 				elseif($packsDtls[$i]['pid']==$user_details['packagechoose'] && $user_details['cardsend']==0)
 					echo 'RENEW';
 				else
-					echo 'BUY NOW'; 
+					echo 'BUY NOW'; 				
 				
-				
-				?>
-				
-				</a></div>
-            </div>
-	    
-            <div class="bnank_p">&nbsp;</div>
-	    <?php }?>
-	    
-        </div>
-	
-	
-   </div>
+			?>
+		
+		</div></a></div><!--button-->
+</div><!--acc-->
+
+ <?php }?>
+
+
+<div class="clear"></div>
+</div><!--package_set-->
+
+<div class="clear"></div>
+</div><!--package-->
+<div class="clear"></div>
 </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
